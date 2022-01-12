708f4ad87956132cb07805aeea37bd57521a9c72
sinka27 <sinka27392@gmail.com>
2022-01-11 16:23:10 -0800

    Call OpenWeather API and display related weather details

--- Assets/script.js
+++ Assets/script.js
@@ -11,11 +11,60 @@ var cityFormEl=document.querySelector("#city-search-form");
 var getWeather = function(){
     var cityName = $("#cityInput").val();
         fetch('http://api.openweathermap.org/data/2.5/weather?q='+cityName+'&units=imperial&APPID=ba0a6d61f2ea067651161c344b40f248')
-        .then(response => response.json())
-        .then(data => console.log(data));
+        .then(function(response){
+            response.json().then(function(data){
+                displayWeather(data, cityName);
     
-}
+});
+        });
+    }
 $("#search").on( "click",getWeather);
+
+//Displaying current city weather
+var displayWeather = function(weather, searchCity){
+    //clear old content
+    weatherContainerEl.textContent= "";  
+    citySearchInputEl.textContent=searchCity;
+ 
+    console.log(weather);
+ 
+    //create date element
+    var currentDate = document.createElement("span")
+    currentDate.textContent=" (" + moment(weather.dt.value).format("MMM D, YYYY") + ") ";
+    citySearchInputEl.appendChild(currentDate);
+ 
+    //create an image element
+    var weatherIcon = document.createElement("img")
+    weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    citySearchInputEl.appendChild(weatherIcon);
+ 
+    //create a span element to hold temperature data
+    var temperatureEl = document.createElement("span");
+    temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+    temperatureEl.classList = "list-group-item"
+   
+    //create a span element to hold Humidity data
+    var humidityEl = document.createElement("span");
+    humidityEl.textContent = "Humidity: " + weather.main.humidity + " %";
+    humidityEl.classList = "list-group-item"
+ 
+    //create a span element to hold Wind data
+    var windSpeedEl = document.createElement("span");
+    windSpeedEl.textContent = "Wind Speed: " + weather.wind.speed + " MPH";
+    windSpeedEl.classList = "list-group-item"
+ 
+    //append to container
+    weatherContainerEl.appendChild(temperatureEl);
+ 
+    //append to container
+    weatherContainerEl.appendChild(humidityEl);
+ 
+    //append to container
+    weatherContainerEl.appendChild(windSpeedEl);
+ 
+    var lat = weather.coord.lat;
+    var lon = weather.coord.lon;
+ }
     
 //uv-index api
 
